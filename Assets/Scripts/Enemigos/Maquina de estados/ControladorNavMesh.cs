767904dfda5c33a7e9c3f3ff449699c8f7075eb8
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2026-02-23 22:33:09 -0300

    Movimiento de enemigos

---
+++ Assets/Scripts/Enemigos/Maquina de estados/ControladorNavMesh.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class ControladorNavMesh : MonoBehaviour {
+
+    [HideInInspector]
+    public Transform perseguirObjectivo;
+
+    private NavMeshAgent navMeshAgent;
+
+	void Awake () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	public void ActualizarPuntoDestinoNavMeshAgent(Vector3 puntoDestino) {
+        navMeshAgent.destination = puntoDestino;
+        navMeshAgent.isStopped = false;
+    }
+
+    public void ActualizarPuntoDestinoNavMeshAgent()
+    {
+        ActualizarPuntoDestinoNavMeshAgent(perseguirObjectivo.position);
+    }
+
+    public void DetenerNavMeshAgent()
+    {
+        navMeshAgent.isStopped = true;
+    }
+
+    public bool HemosLlegado()
+    {
+        return navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance && !navMeshAgent.pathPending;
+    }
+}
